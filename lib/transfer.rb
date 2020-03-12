4ec6438d39c879d4218a9f738c5f00473fb682a1
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-03-12 01:21:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @status == "pending"
+    if @sender.balance > @amount && @status == "pending"
        @sender.balance -= @amount
        @receiver.balance += @amount
        @status = "complete"
