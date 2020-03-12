54af95083ec9fd6126f533551beb76e7417dea1c
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-03-12 01:33:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,8 +24,8 @@ class Transfer
        @sender.balance -= @amount
        @receiver.balance += @amount
        @status = "complete"
-    else
-      @status = "rejected"
+       
+    else  @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
