ccd3d1e3a31a2d83d237d6788d9bd560a8a5d8a7
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-03-12 01:56:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,10 +22,11 @@ class Transfer
    def execute_transaction
     if @sender.balance < @amount
       @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+      puts "Transaction rejected. Please check your account balance."
 
     elsif @status == "complete"
       puts "Transaction was already excuted"
+
     else
       @sender.deposit( @amount * -1 )
       @receiver.deposit( @amount )
